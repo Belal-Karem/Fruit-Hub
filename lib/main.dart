a38ae43f573359d1339c4f0353d0901967dd3c2a
Belal Karem <belalkarem41@gmail.com~>
2025-11-25 16:30:37 +0200

    Splash View ui done

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/helper_functions/on_generate_route.dart';
+import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
+    );
+  }
+}
