fbe73d98382eea6ead11262fd1fc0e2ac3b1945c
Belal Karem <belalkarem41@gmail.com~>
2026-02-22 22:41:59 +0200

    create custom button

--- lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widget/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/constants.dart';
 import 'package:fruit_hub/core/utils/app_color.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/views/widget/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -18,6 +20,12 @@ class OnBoardingViewBody extends StatelessWidget {
             color: AppColor.primaryColor.withAlpha(127),
           ),
         ),
+        const SizedBox(height: 29),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
+        ),
+        const SizedBox(height: 43),
       ],
     );
   }
