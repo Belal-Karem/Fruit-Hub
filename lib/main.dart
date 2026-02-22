4f083ce2144417b2df5ecfd0af6546a5b6d0e17d
Belal Karem <belalkarem41@gmail.com~>
2026-02-22 03:15:08 +0200

    create git init and Arabic localization and change app name, icon

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:fruit_hub/core/helper_functions/on_generate_route.dart';
+import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
+
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
+    );
+  }
+}
