68a4092012541a26a33448051f66e082d3461f44
Belal Karem <belalkarem41@gmail.com~>
2026-02-22 03:18:46 +0200

    create on boarding view

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesCircles
+  /// assets/images/Circles.svg
+  static const String imagesCircles = "assets/images/Circles.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/Logo.svg
+  static const String imagesLogo = "assets/images/Logo.svg";
+
+  /// Assets for imagesPageViewItem1BackgroundImage
+  /// assets/images/page_view_item1_background_Image.svg
+  static const String imagesPageViewItem1BackgroundImage = "assets/images/page_view_item1_background_Image.svg";
+
+  /// Assets for imagesPageViewItem1Image
+  /// assets/images/page_view_item1_Image.svg
+  static const String imagesPageViewItem1Image = "assets/images/page_view_item1_Image.svg";
+
+  /// Assets for imagesPageViewItem2BackgroundImage
+  /// assets/images/page_view_item2_background_Image.svg
+  static const String imagesPageViewItem2BackgroundImage = "assets/images/page_view_item2_background_Image.svg";
+
+  /// Assets for imagesPageViewItem2Image
+  /// assets/images/page_view_item2_Image.svg
+  static const String imagesPageViewItem2Image = "assets/images/page_view_item2_Image.svg";
+
+  /// Assets for imagesPlant
+  /// assets/images/Plant.svg
+  static const String imagesPlant = "assets/images/Plant.svg";
+}
+
---
+++ lib/features/on_boarding/presentation/views/widget/page_view_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+
+class PageViewItem extends StatelessWidget {
+  const PageViewItem({
+    super.key,
+    required this.image,
+    required this.backgroundImage,
+    required this.subtitle,
+    required this.title,
+  });
+
+  final String image, backgroundImage;
+  final String subtitle;
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: MediaQuery.sizeOf(context).height * 0.5,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: SvgPicture.asset(backgroundImage, fit: BoxFit.fill),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: SvgPicture.asset(image),
+              ),
+              Text('تخطي'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
