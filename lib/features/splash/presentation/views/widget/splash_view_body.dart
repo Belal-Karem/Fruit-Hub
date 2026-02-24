816e56b78cb5e3f722b4e5f6298b9a925fa2c4df
Belal Karem <belalkarem41@gmail.com~>
2026-02-24 17:17:59 +0200

    add animation to splash view

--- lib/features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -10,26 +10,71 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> {
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+  late Animation<double> _plantSize;
+  late Animation<double> _circleHeight;
+
   @override
   void initState() {
-    excuteNaviagtion();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _opacity = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.3, 0.8, curve: Curves.easeIn),
+      ),
+    );
+
+    _plantSize = Tween<double>(
+      begin: 80,
+      end: 140,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+
+    _circleHeight = Tween<double>(
+      begin: 100,
+      end: 200,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+    // excuteNaviagtion();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [SvgPicture.asset(Assets.imagesPlant)],
-        ),
-        SvgPicture.asset(Assets.imagesLogo),
-        SvgPicture.asset(Assets.imagesCircles, fit: BoxFit.fill),
-      ],
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: SizedBox(
+                width: _plantSize.value,
+                height: _plantSize.value,
+                child: SvgPicture.asset(Assets.imagesPlant),
+              ),
+            ),
+
+            Opacity(
+              opacity: _opacity.value,
+              child: SvgPicture.asset(Assets.imagesLogo),
+            ),
+
+            SizedBox(
+              height: _circleHeight.value,
+              child: SvgPicture.asset(Assets.imagesCircles, fit: BoxFit.fill),
+            ),
+          ],
+        );
+      },
     );
   }
 
