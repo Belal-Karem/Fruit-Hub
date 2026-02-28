100bcee33f0d024b2e9fc73eb04798efe35eafe4
Belal Karem <belalkarem41@gmail.com~>
2026-03-01 01:37:11 +0200

    Added light primary color

--- lib/core/utils/styles/app_color.dart
+++ lib/core/utils/styles/app_color.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 
 abstract class AppColor {
   static const Color primaryColor = Color(0xff1B5E37);
+  static const Color lightPrimaryColor = Color(0xFF2D9F5D);
   static const Color secondaryColor = Color(0xffF4A91F);
   static const Color scaffoldBackgroundColor = Colors.white;
 }
