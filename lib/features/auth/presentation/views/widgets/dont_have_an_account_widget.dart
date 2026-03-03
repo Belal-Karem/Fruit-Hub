1b100f13bb27377ab677242d16c269e77f22363b
Belal Karem <belalkarem41@gmail.com~>
2026-03-03 16:06:38 +0200

    `Added custom app bar and signup view body to signup view, updated dont have an account widget to navigate to signup view`

--- lib/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart
+++ lib/features/auth/presentation/views/widgets/dont_have_an_account_widget.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/utils/styles/app_color.dart';
 import 'package:fruit_hub/core/utils/styles/app_text_style.dart';
+import 'package:fruit_hub/features/auth/presentation/views/signup_view.dart';
 
 class DontHaveAnAccountWidget extends StatelessWidget {
   const DontHaveAnAccountWidget({super.key});
@@ -13,6 +15,9 @@ class DontHaveAnAccountWidget extends StatelessWidget {
         style: AppTextStyle.semiBold16.copyWith(color: const Color(0xff616A6B)),
         children: [
           TextSpan(
+            recognizer: TapGestureRecognizer()
+              ..onTap = () =>
+                  Navigator.pushNamed(context, SignupView.routeName),
             text: 'قم بإنشاء حساب',
             style: AppTextStyle.bold13.copyWith(color: AppColor.primaryColor),
           ),
