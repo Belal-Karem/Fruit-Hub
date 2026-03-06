722bae6e94998755e2e06c016946ef61cb559793
Belal Karem <belalkarem41@gmail.com~>
2026-03-06 23:05:43 +0200

    create get it services

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,12 @@
+import 'package:fruit_hub/core/services/firebase_auth_service.dart';
+import 'package:fruit_hub/features/auth/data/repos/auth_repo_imle.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setup() {
+  getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImle(firebaseAuthService: getIt<FirebaseAuthService>()),
+  );
+}
