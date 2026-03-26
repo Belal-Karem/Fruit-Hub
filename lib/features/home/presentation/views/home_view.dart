ad17691dea443d192d137572786df55e7ab63f6c
Belal Karem <belalkarem41@gmail.com~>
2026-03-26 19:10:18 +0200

    create home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  static const String routeName = 'home';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold();
