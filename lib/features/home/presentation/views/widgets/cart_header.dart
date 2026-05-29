5bdfa7e1f9a802dd408462ec0c618f63bd58a35c
Belal Karem <belalkarem41@gmail.com>
2026-05-29 16:47:46 +0200

    Display number of products added

--- lib/features/home/presentation/views/widgets/cart_header.dart
+++ lib/features/home/presentation/views/widgets/cart_header.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/utils/theme/app_text_style.dart';
+import 'package:fruit_hub/features/home/presentation/manager/cart_cubit/cart_cubit.dart';
 
 class CartHeader extends StatelessWidget {
   const CartHeader({super.key});
@@ -13,7 +15,7 @@ class CartHeader extends StatelessWidget {
       decoration: BoxDecoration(color: const Color(0xFFEBF9F1)),
       child: Center(
         child: Text(
-          'لديك 3 منتجات في سله التسوق',
+          'لديك ${context.watch<CartCubit>().cartEntity.cartItems.length} منتجات في سله التسوق',
           style: AppTextStyle.regular13.copyWith(
             color: const Color(0xFF1B5E37),
           ),
