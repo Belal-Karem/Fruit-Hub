e5a02b4256c77c4a96df3be5c46220af76469b24
Belal Karem <belalkarem41@gmail.com>
2026-06-17 20:04:49 +0200

    Create order address and shipping address entity

---
+++ lib/features/checkout/domain/entites/shipping_address_entity.dart
@@ -0,0 +1,17 @@
+class ShippingAddressEntity {
+  final String name;
+  final String phone;
+  final String email;
+  final String address;
+  final String city;
+  final String floorNumber;
+
+  ShippingAddressEntity(
+    this.name,
+    this.phone,
+    this.email,
+    this.address,
+    this.city,
+    this.floorNumber,
+  );
+}
