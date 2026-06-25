20cad7c252f19643a59eceffe8fe104c993ce309
Belal Karem <belalkarem41@gmail.com>
2026-06-25 09:48:07 +0200

    Remove app_key.dart from tracking

--- lib/core/utils/app_key.dart
+++
@@ -1,4 +0,0 @@
-const kPaypalSecretKey =
-    'EEAa4AvcptDslRE9_QgWkglM6U8qtk0X21tAH_5WBKnTUN-kec-voT3TQ13_9hKlaQ4Zai9iEpjA3UHn';
-const kPaypalClientId =
-    'AZmdc7Bwp6A_WG2gP79Dj7ZxsE4xYZiIEeTA5hA0oGoBcvVGpucEFtxmLpeYCyZGZaslYdn-7NS0KOVA';
